abeb11f5ad5683a887184949231ab175b4691bc5
xx <zz@gmail.com>
2020-04-09 14:38:25 +0300

    up on folders tree + add tests

---
+++ Src/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondRepoCore
+{
+    public class Car
+    {
+        public int Prepair(string engineName)
+        {
+            return -1;
+        }
+    }
+}
---
+++ Tests/BankAccountTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecondRepoCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondRepoCore.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DebitTest()
+        {
+            BankAccount ba = new BankAccount("ddd", 0);
+            ba.Debit(222);
+            
+        }
+    }
+}
